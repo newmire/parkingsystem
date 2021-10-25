625de5a77229ad2a8601fcc79c80a5b3ef22c42e
Rémi Bouchard <newmire@hotmail.fr>
2021-10-25 18:57:49 +0200

    30 minutes gratuite offerte installer

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,6 +1,7 @@
 package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.model.Ticket;
 
 import java.time.LocalDateTime;
@@ -9,33 +10,48 @@ import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class FareCalculatorService {
+    public TicketDAO ticketDAO = new TicketDAO();
 
-        private LocalDateTime convert(Date date){
-            return date.toInstant()
-                    .atZone(ZoneId.systemDefault())
-                    .toLocalDateTime();
+    private LocalDateTime convert(Date date) {
+        return date.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
+
+    public String conditionRegularUser(){
+        ticketDAO.getTicket(String.vehicleRegNumber)
+
+    }
+    public boolean regularUser(){
+        if(){
+
         }
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+    }
+*/
+    public void calculateFare(Ticket ticket) {
+        if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
 
         LocalDateTime inHour = convert(ticket.getInTime());
         LocalDateTime outHour = convert(ticket.getOutTime());
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        long duration = ChronoUnit.MINUTES.between(inHour, outHour);
-
-        switch (ticket.getParkingSpot().getParkingType()){
+        long duration = ChronoUnit.MINUTES.between(inHour, outHour)-30;
+             duration = duration  < 0 ? 0 : duration;
+        switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
-                ticket.setPrice(duration * (Fare.CAR_RATE_PER_HOUR/60));
+                    ticket.setPrice(duration * (Fare.CAR_RATE_PER_HOUR / 60));
+                }
                 break;
-            }
+
             case BIKE: {
-                ticket.setPrice(duration * (Fare.BIKE_RATE_PER_HOUR/60));
+                    ticket.setPrice(duration * (Fare.BIKE_RATE_PER_HOUR / 60));
+                }
                 break;
-            }
-            default: throw new IllegalArgumentException("Unknown Parking Type");
+
+            default:
+                throw new IllegalArgumentException("Unknown Parking Type");
         }
     }
 }
